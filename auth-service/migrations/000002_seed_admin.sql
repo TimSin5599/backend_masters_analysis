e62430895f4bca3871cbed07bf2dcbfb962cf267
TimSin5599 <timasinitsyn@bk.ru>
2026-04-25 22:15:46 +0300

    fix: reset old auth migration

--- auth-service/migrations/000002_seed_admin.sql
+++ auth-service/migrations/000002_seed_admin.sql
@@ -1,4 +1,4 @@
 -- Migration to add initial admin user
-INSERT INTO users (email, password, role, first_name, last_name) 
-VALUES ('admin@masters.com', '$2a$10$aUrOVtWUX2sxq593sHw3/uMFwZWWmUeYQmSs1UDqRhmbEb1wRqVBu', 'admin', 'Admin', 'Admin')
+INSERT INTO users (email, password, role) 
+VALUES ('admin@masters.com', '$2a$10$aUrOVtWUX2sxq593sHw3/uMFwZWWmUeYQmSs1UDqRhmbEb1wRqVBu', 'admin')
 ON CONFLICT (email) DO NOTHING;
